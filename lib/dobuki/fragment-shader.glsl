4119c661b746220ce23f353f011d08ead378a95e
Jack Le Hamster <jacklehamster@gmail.com>
2016-12-28 06:46:17 -0800

    Disaster

--- lib/dobuki/fragment-shader.glsl
+++ lib/dobuki/fragment-shader.glsl
@@ -3,10 +3,16 @@
 uniform sampler2D texture[ 16 ];
 varying vec2 vUv;
 varying vec4 vCut;
+varying float vTex;
 
 void main() {
     vec2 uv = vUv;
     uv.x = vCut.x + uv.x * vCut.z;
     uv.y = vCut.y + uv.y * vCut.w;
-    gl_FragColor = texture2D( texture[0],  vec2(uv.x, uv.y));
+    for(int i=0; i<16; i++) {
+        if(i == int(vTex)) {
+            gl_FragColor = texture2D( texture[i],  vec2(uv.x, uv.y));
+            break;
+        }
+    }
 }
