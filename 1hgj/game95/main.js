ecc8cee3811983f80cb782a6d2285ee35a8a30f6
Jack Le Hamster <jacklehamster@gmail.com>
2017-02-18 14:36:48 -0800

    game 95

---
+++ 1hgj/game95/main.js
@@ -0,0 +1,144 @@
+window.onload = function() {
+    var renderer = new THREE.WebGLRenderer();
+    renderer.setSize( innerWidth, innerHeight );
+    var camera = new THREE.OrthographicCamera(-innerWidth/2, innerWidth/2, innerHeight/2, -innerHeight/2, 0.1, THREE.Infinity );
+    var scene = new THREE.Scene();
+    window.addEventListener("resize",
+        function(e) {
+            camera.left = -innerWidth/2;
+            camera.right = innerWidth/2;
+            camera.top = innerHeight/2;
+            camera.bottom = -innerHeight/2;
+            camera.updateProjectionMatrix();
+        }
+    );
+
+    var maps = [
+        THREE.ImageUtils.loadTexture('cat_0.png'),
+        THREE.ImageUtils.loadTexture('cat_1.png'),
+        THREE.ImageUtils.loadTexture('cat_2.png'),
+        THREE.ImageUtils.loadTexture('cat_3.png'),
+    ];
+    var mats = maps.map(
+        function(map) {
+            return new THREE.MeshBasicMaterial({
+                map:map,
+            });
+        }
+    );
+
+    var cat = new THREE.Mesh(
+        new THREE.PlaneGeometry( 100, 100), mats[0]
+    );
+    cat.position.set(-200,-100,0);
+    var dy = 0;
+
+    scene.add(cat);
+    camera.position.set(0,0,400);
+    document.body.appendChild( renderer.domElement );
+
+    var blocks = [
+        new THREE.MeshBasicMaterial( {color: 0x774444, side: THREE.DoubleSide}),
+        new THREE.MeshBasicMaterial( {color: 0x774444, side: THREE.DoubleSide}),
+        new THREE.MeshBasicMaterial( {color: 0x774444, side: THREE.DoubleSide}),
+    ];
+    blocks = blocks.map(
+        function(mat) {
+            return new THREE.Mesh(
+                new THREE.PlaneGeometry(50,50),
+                mat
+            )
+        }
+    );
+    blocks.forEach(function(block) {
+        scene.add(block);
+    });
+
+    var ground = new THREE.Mesh(
+        new THREE.PlaneGeometry( innerWidth*10, 100),
+        new THREE.MeshBasicMaterial( {color: 0x777777, side: THREE.DoubleSide})
+    );
+    ground.position.set(0,-175,0);
+    scene.add(ground);
+
+    function restart() {
+        blocks.forEach(
+            function(block, index) {
+                block.position.set(innerWidth/2+index*500,-100,0);
+            }
+        );
+    }
+
+    function shake() {
+
+    }
+
+
+    var doubleJump = 2;
+    document.addEventListener("keydown",
+        function(e) {
+            if(paused && previousTime-paused > 500) {
+                s = previousTime;
+                paused = 0;
+                restart();
+                return;
+            }
+            if(cat.position.y<=-100 || doubleJump) {
+                doubleJump --;
+                dy = 15;
+            }
+    });
+//    Object3D.rotateOnAxis( axis, angle );
+    var axis = new THREE.Vector3(0,0,-1);
+    var paused = 1;
+
+    var s = 0;
+    var previousTime = 0;
+    function loop(time) {
+        requestAnimationFrame( loop );
+
+        if(!paused) {
+            var frame = Math.floor(time/50);
+            cat.material = mats[frame%mats.length];
+            cat.position.y += dy;
+            cat.setRotationFromAxisAngle(axis, -dy/30);
+            dy --;
+            if(cat.position.y<=-100) {
+                cat.position.y = -100;
+                dy = 0;
+                doubleJump = 2;
+            }
+
+            var speed = (cat.position.y<=-100 ? 1 : 2) + (time-s)/10000;
+            blocks.forEach(
+                function(block) {
+                    block.position.x -= (time-previousTime)/3 * speed;
+                    if(block.position.x < -innerWidth/2) {
+                        block.position.x += innerWidth + 500*Math.random();
+                        block.scale.y = .5 + Math.random();
+                    }
+                    var dx = cat.position.x - block.position.x;
+                    var dy = cat.position.y - block.position.y;
+                    var dist = dx*dx+dy*dy;
+                    if(dist<50) {
+                        paused = time;
+                    }
+                }
+            );
+            score.textContent = "" + Math.floor((time-s)/100)*10;
+        }
+
+        renderer.render(scene,camera);
+        previousTime = time;
+    }
+    loop(0);
+
+
+    var score = document.createElement("span");
+    score.style.color = "white";
+    score.style.position = "absolute";
+    score.style.left = "40px";
+    score.style.top = "40px";
+    score.textContent = "SPACE to Jump/double-jump";
+    document.body.appendChild(score);
+};
